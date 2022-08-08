f2e821f14d9fb6ba047b205c8d59e0e2637b10a9
Dahud Yusuf <abiodunyusuf4@gmail.com>
2022-08-08 18:07:37 +0100

    Loops

--- 06_Day_Loops/main.js
+++ 06_Day_Loops/main.js
@@ -119,31 +119,6 @@ const numbers = [1, 2, 3, 4, 5];
 // }
 
 // Exercises: Level 1
-const countries = [
-  "Albania",
-  "Bolivia",
-  "Canada",
-  "Denmark",
-  "Ethiopia",
-  "Finland",
-  "Germany",
-  "Hungary",
-  "Ireland",
-  "Japan",
-  "Kenya",
-];
-
-const webTechs = [
-  "HTML",
-  "CSS",
-  "JavaScript",
-  "React",
-  "Redux",
-  "Node",
-  "MongoDB",
-];
-
-const mernStack = ["MongoDB", "Express", "React", "Node"];
 // 1: Iterate 0 to 10 using for loop, do the same using while and do while loop
 // for (i = 0; i<= 10; i++) {
 //     console.log(i)
@@ -326,30 +301,6 @@ arr.push(sum1);
 console.log(arr);
 */
 
-// 13: Develop a small script which generate array of 5 random numbers
-// let arr = []
-// for (num = 0; num <= 6; num++){
-//   if ( num = Math.floor(Math.random() * 10) + 1)
-//   num++
-// }
-
-// arr.push(num)
-// console.log(`${arr}, ${arr}, ${arr}`)
-
-var arr = [];
-while(arr.length < 5){
-    var r = Math.floor(Math.random() * 100) + 1;
-    if(arr.indexOf(r) === -1) arr.push(r);
-}
-console.log(arr);
-
-// 14: Develop a small script which generate array of 5 random numbers and the numbers must be unique
-
-
-
-
-
-
 // for loop that goes from top to bottom
 // var total = 0;
 // for (var num = 100; num >= 0; num--) {
@@ -389,23 +340,199 @@ console.log(arr);
 //     console.log(randomStuff[x])
 //     x--
 //   }
+// 13: Develop a small script which generate array of 5 random numbers
+// let arr = []
+// for (num = 0; num <= 6; num++){
+//   if ( num = Math.floor(Math.random() * 10) + 1)
+//   num++
+// }
+
+// arr.push(num)
+// console.log(`${arr}, ${arr}, ${arr}`)
+
+// var arr = [];
+// while(arr.length < 5){
+//     var r = Math.floor(Math.random() * 100) + 1;
+//     if(arr.indexOf(r) === -1) arr.push(r);
+// }
+// console.log(arr);
+
+// 14: Develop a small script which generate array of 5 random numbers and the numbers must be unique
+
+// 15. Develop a small script which generate a six characters random id:
+
+// let array = "qawsedrjjfhhgjjejnnchhutitjuhhh QWERTYIIIJFKLSMNBNVNN##&*$$**@(($&$*($&&%&%**$(($(?><>ieje.".split("")
+// // console.log(array)
+// function generator (passwordLength) {
+//   let password = ""
+// for (i = 0; i < passwordLength; i++){
+//   let randomnumber = Math.floor(Math.random() * array.length
+//   )
+//   password += array[randomnumber]
+// }
+// return password
+// }
+// console.log(generator(8))
 
 
 
+// const number = [1, 2, 3, 4, 5]
+// const newArr = []
+// let sum = 0
+// for (let i = 0; i< numbers.length; i++){
+//   newArr.push(number[i] ** 2)
+// }
+
+// console.log(newArr)
+
+
+// let i = 0
+// while (i <= 5) {
+//   console.log(i)
+//   i++
+// }
+
+
+// let i = 0
+// do {
+//   console.log(i)
+//   i++
+// } while (i <= 5)
+
+// Develop a small script which generate any number of characters random id:
+  // fe3jo1gl124g
+  // xkqci4utda1lmbelpkm03rba
+
+// let num1 = Math.floor(Math.random() * 718)
+// console.log(num1)
+
+// let num2 = 
+
+/*
+// 1 Declare a variable named challenge and assign it to an initial value '30 Days Of JavaScript'.
+let challenge = '30 Days Of JavaScript'
+console.log(challenge)
+console.log(challenge.length)
+
+// challenge =  challenge.toUpperCase()
+console.log(challenge.toUpperCase())
+console.log(challenge.toLowerCase())
+
+
+// challenge = challenge.substring(3)
+// console.log(challenge)
+// console.log(challenge.substring(0, 2))
+// challenge = challenge.includes('Script')
+// console.log(challenge)
+
+// console.log(challenge.split(' '))
+// console.log(challenge.split(''))
+
+// let companies = 'Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon' 
+// console.log(companies.split(', '))
+// challenge = challenge.replace('JavaScript','Python')
+// console.log(challenge)
+
+// console.log(challenge.charAt(15))
+// console.log(challenge.charCodeAt(11))
+// console.log(challenge.indexOf('a'))
+// console.log(challenge.lastIndexOf('a'))
+
+let word = 'You cannot end a sentence with because because because is a conjunction'
+console.log(word.indexOf('because'))
+console.log(word.lastIndexOf('because'))
+console.log(word.search('because'))
+
+*/
+
+const countries = [
+  "Albania",
+  "Bolivia",
+  "Canada",
+  "Denmark",
+  "Ethiopia",
+  "Finland",
+  "Germany",
+  "Hungary",
+  "Ireland",
+  "Japan",
+  "Kenya",
+];
+
+const webTechs = [
+  "HTML",
+  "CSS",
+  "JavaScript",
+  "React",
+  "Redux",
+  "Node",
+  "MongoDB",
+];
+
+const mernStack = ["MongoDB", "Express", "React", "Node"];
+
+
+// Exercise: Level 2
+let arr1 = []
+// No 4: Using the above countries array, create the following new array.
+for (let i = 0; i < countries.length; i++){
+  arr1.push(countries[i])
+}  
+
+// console.log(arr1)
+ 
+
+// 5: Using the above countries array, create an array for countries length'
+// console.log(countries[0].length)
+/*let arr = []
+for (let i = 0; i < countries.length; i++){
+  arr.push(countries[i].length)
+}*/
+// console.log(arr)
+
+// 6: Use the countries array to create the following array of arrays:
+/*
+let arr2 = []
+let arr3 = []
+let arr4 = []
+for (let i = 0; i < countries.length; i++) {
+  arr2.push(countries[i])
+  arr3.push(countries[i].slice(0, 3))
+  arr4.push(countries[i].length)
+
+}
+for (let i = 0; i < countries.length; i++) {
+let newArr = arr2[i] + "', '" + arr3[i]  + "', '" + arr4[i] 
+
+console.log(`['${newArr}'],`)
+  
+}
+*/
+// 7: In above countries array, check if there is a country or countries containing the word 'land'. If there are countries containing 'land', print it as array. If there is no country containing the word 'land', print 'All these countries are without land'.
 
 
 
+// let index = countries.indexOf('land')  // 0
+// console.log(countries.find('land'))
+// if(index === -1){
+//    console.log('This fruit does not exist in the array')  
+// } else {
+//     console.log('This fruit does exist in the array')
+// }
 
+const myString = "hello world!"
+if(myString.includes("hello")){
+  // do something 
+  console.log("hello")
+}else{
+  //also do something
+}
 
+if(countries.includes("land")) {
+  console.log(countries.includes("land"))
+} else {
+  console.log(countries)
+}
 
-
-
-
-
-
-
-
-
-
-
+console.log(countries.includes('land'))
 
